4ee1349a49301c0e535bef1601e16eabc57c0934
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2014-12-27 19:56:09 +0300

    Add description for properties adapter

--- ARHandlingChains/ARHandlingChains/PropertiesDescriptors/Tools/PropertiesAdapter/ARHCMutableDictionaryPropertiesAdapter.m
+++ ARHandlingChains/ARHandlingChains/PropertiesDescriptors/Tools/PropertiesAdapter/ARHCMutableDictionaryPropertiesAdapter.m
@@ -9,18 +9,15 @@
 
 static NSString *const kMutableDictionaryAutomaticAdapterPresentedSuffix = @"Presented";
 
-@implementation ARHCMutableDictionaryPropertiesAdapter
-{
+@implementation ARHCMutableDictionaryPropertiesAdapter {
 @private
     NSMutableDictionary *_state;
     CPropertiesDescriptorsAccessorsHolder *_holder;
 }
 
-- (instancetype)initWithDictionary:(NSMutableDictionary *)state
-{
+- (instancetype)initWithDictionary:(NSMutableDictionary *)state {
     self = [super init];
-    if (self)
-    {
+    if (self) {
         _state = state;
         _holder = [CPropertiesDescriptorsAccessorsHolder holder];
     }
@@ -30,17 +27,24 @@ static NSString *const kMutableDictionaryAutomaticAdapterPresentedSuffix = @"Pre
 
 @synthesize state = _state;
 
+- (NSString *)description {
+    NSMutableString *description = [NSMutableString stringWithFormat:@"<%@: ",
+                                                                     NSStringFromClass([self class])];
+
+    [description appendString:@"content: "];
+    [description appendString:[self.state description]];
+    [description appendString:@">"];
+    return description;
+}
+
 #pragma mark - Forwarding support
 
-- (BOOL)respondsToSelector:(SEL)aSelector
-{
+- (BOOL)respondsToSelector:(SEL)aSelector {
     BOOL result = [super respondsToSelector:aSelector];
-    if (!result)
-    {
-        result = [_holder.accessors objectForKey:NSStringFromSelector (aSelector)] != nil;
+    if (!result) {
+        result = [_holder.accessors objectForKey:NSStringFromSelector(aSelector)] != nil;
     }
-    else
-    {
+    else {
         NSLog(@"MutableDictionaryAdapter can't detect selector %@. \n"
                 "\nMay be you forgot adapt some class by corresponding propertyDescriptor or "
                 "note propertyDescriptor in source code through @protocol() link", NSStringFromSelector(aSelector));
@@ -48,36 +52,29 @@ static NSString *const kMutableDictionaryAutomaticAdapterPresentedSuffix = @"Pre
     return result;
 }
 
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
-{
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
     NSMethodSignature *result = [super methodSignatureForSelector:aSelector];
 
-    id <IAdaptedPropertyAccessor> accessor = [_holder.accessors objectForKey:NSStringFromSelector (aSelector)];
-    if (accessor != nil)
-    {
+    id <IAdaptedPropertyAccessor> accessor = [_holder.accessors objectForKey:NSStringFromSelector(aSelector)];
+    if (accessor != nil) {
         result = accessor.signature;
     }
-    else
-    {
+    else {
         NSLog(@"MutableDictionaryAdapter can't detect selector %@. \n"
                 "\nMay be you forgot adapt some class by propertyDescriptor or "
                 "note propertyDescriptor in source code through @protocol() link", NSStringFromSelector(aSelector));
-
     }
 
     return result;
 }
 
-- (void)forwardInvocation:(NSInvocation *)anInvocation
-{
-    id <IAdaptedPropertyAccessor> accessor = [_holder.accessors objectForKey:NSStringFromSelector (anInvocation.selector)];
-    if (accessor != nil)
-    {
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    id <IAdaptedPropertyAccessor> accessor = [_holder.accessors objectForKey:NSStringFromSelector(anInvocation.selector)];
+    if (accessor != nil) {
         [accessor performWithInvocation:anInvocation
                                delegate:_state];
     }
-    else
-    {
+    else {
         NSLog(@"MutableDictionaryAdapter can't detect invocation %@. \n"
                 "\nMay be you forgot adapt some class by propertyDescriptor or "
                 "note propertyDescriptor in source code through @protocol() link", anInvocation);
@@ -85,8 +82,7 @@ static NSString *const kMutableDictionaryAutomaticAdapterPresentedSuffix = @"Pre
     }
 }
 
-- (id)copyWithZone:(NSZone *)zone
-{
+- (id)copyWithZone:(NSZone *)zone {
     NSMutableDictionary *copyOfState = [_state mutableCopy];
     ARHCMutableDictionaryPropertiesAdapter *result = [[ARHCMutableDictionaryPropertiesAdapter alloc] initWithDictionary:copyOfState];
     return result;
